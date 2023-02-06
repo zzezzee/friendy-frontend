3c54ec0e77c75ed28f550a2d83e662accda746c9
zzezzee <GitHub zzezzee@naver.com>
2023-02-06 16:15:34 +0900

    사용자검색

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import { userApiService } from '../services/UserApiService';
 import Store from './Store';
 
@@ -9,6 +10,8 @@ export default class UserStore extends Store {
     this.relationship = '';
 
     this.users = [];
+
+    this.filteredUsers = [];
     this.searching = false;
   }
 
@@ -36,8 +39,6 @@ export default class UserStore extends Store {
   async fetchUsers() {
     const { users } = await userApiService.fetchUsers();
 
-    console.log(`data${users}`);
-
     this.users = users;
 
     this.publish();
@@ -55,6 +56,19 @@ export default class UserStore extends Store {
     } catch (e) {
       return '';
     }
+  }
+
+  filterUsersWithNickname(nickname) {
+    nickname.length === 0
+      ? this.searching = false
+      : this.searching = true;
+
+    this.filteredUsers = this.users.filter((user) => (
+      user.nickname.includes(nickname)
+    ));
+
+    this.publish();
+    console.log(this.searching);
   }
 }
 
